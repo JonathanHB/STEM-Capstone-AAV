11cced62f556ecf1df4216e39298e628241d939c
jonathan <jonathan3141@gmail.com>
2017-02-15 09:29:04 -0500

    fixed several serious bugs, implemented regen_point method

--- LK_opticalflow.py
+++ LK_opticalflow.py
@@ -28,27 +28,35 @@ class Hoop_finder:
 		self.lastframe = np.zeros((360, 640), dtype = "uint8")
 		self.lastflow = np.zeros((360, 640, 2), dtype = "uint8")
 
-		self.oldgray = np.zeros((360, 640), dtype = "uint8")
+		self.old_gray = np.zeros((360, 640), dtype = "uint8")
 		self.mask = np.zeros((360, 640, 3), dtype = "uint8")
 
+		self.p0 = []
+
+		# Create some random colors for each trail
+		self.color = np.random.randint(0,255,(100,3))
+		# Parameters for lucas kanade optical flow, used repeatedly
+		self.lk_params = dict(winSize  = (15,15),maxLevel = 2,criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+		# params for ShiTomasi corner detection, used once
+		self.feature_params = dict(maxCorners = 100,qualityLevel = 0.3,minDistance = 7,blockSize = 7)
+		self.feature_params2 = dict(maxCorners = 1,qualityLevel = 0.3,minDistance = 7,blockSize = 7)
+
 		self.initframe = True		
-		#self.n = 0
+		self.n = 0
 
 	def takeimage(self, img): #runs image processing to find hula hoop, and feeds the resulting drone and hoop pose data into the navigation algorithm
-		#if (self.n*.5).is_integer(): 
-
-		if self.initframe:
+		if (self.initframe): 
 
 			self.initflow(img)
+			self.initframe = False
 
-		else:
-
-			self.getvectors(self.processimage2(img)) #posedata = 
+		self.processimage2(img)
+			
 		#self.hoopnav(posedata)	
-		#self.n+=1
+		##self.n+=1
 	
 	
-	def odometry(self, data): #unimplemented; the odometry is itself optical flow based
+	def odometry(self, data): #unused; the odometry is itself optical flow based
 		#stores odometry data to integrate with vision processing
 		#print data
 		#print data.twist.twist.linear.y
@@ -76,18 +84,40 @@ class Hoop_finder:
 		print totalrad/(230400/(scale*scale))
 		
 
-	def self.initflow(self, imgdrone1):
+	def regenpoints(self, points, gray):
 
-		# params for ShiTomasi corner detection, used once
-		feature_params = dict( maxCorners = 100,qualityLevel = 0.3,minDistance = 7,blockSize = 7 )
-		# Parameters for lucas kanade optical flow, used repeatedly
-		lk_params = dict( winSize  = (15,15),maxLevel = 2,criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-		# Create some random colors for each trail
-		color = np.random.randint(0,255,(100,3))
+		borderwidth = 10
+		imagex = 360
+		imagey = 640
+
+		upperx = imagex-borderwidth
+		uppery = imagey-borderwidth
+		
+		movement = [0,0]
+
+		for p in points:
+
+			if p[0][1] > upperx or p[0][1] < borderwidth or p[0][0] > uppery or p[0][0] < borderwidth:
+
+				movement[0] -= p[0][1]
+				movement[1] -= p[0][0]
+				#print p
+				p = cv2.goodFeaturesToTrack(self.old_gray, mask = None, **self.feature_params2)[0]
+
+				movement[0] += p[0][1]
+				movement[1] += p[0][0]
+				#print p
+		
+		#print movement
+		return movement,points
+
+
+	def initflow(self, imgdrone1): #generates feature point array for LK optical flow
+	
 		# Take first frame and find corners in it
 		imgbgr = self.bridge.imgmsg_to_cv2(imgdrone1, "bgr8")
 		self.old_gray = cv2.cvtColor(imgbgr, cv2.COLOR_BGR2GRAY)
-		p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+		self.p0 = cv2.goodFeaturesToTrack(self.old_gray, mask = None, **self.feature_params)
 		# Create a mask image for drawing purposes
 		self.mask = np.zeros_like(imgbgr)
 
@@ -96,17 +126,38 @@ class Hoop_finder:
 		imgbgr = self.bridge.imgmsg_to_cv2(imgdrone1, "bgr8")
 		frame_gray = cv2.cvtColor(imgbgr, cv2.COLOR_BGR2GRAY)
 		# calculate optical flow
-		p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+		p1, st, err = cv2.calcOpticalFlowPyrLK(self.old_gray, frame_gray, self.p0, None, **self.lk_params)
 		# Select good points
+
+		regen_movement, p1 = self.regenpoints(p1, frame_gray) #moves points that have drifted to the edges, returns the error that this movement will induce in flow calculations so that it can be cancelled out
+
+		
+
 		good_new = p1[st==1]
-		good_old = p0[st==1]
+		good_old = self.p0[st==1]
 		# draw the tracks
     		for i,(new,old) in enumerate(zip(good_new,good_old)):
         		a,b = new.ravel()
         		c,d = old.ravel()
-        		mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-        		frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
-    		img = cv2.add(frame,mask)
+        		mask = cv2.line(self.mask, (a,b),(c,d), self.color[i].tolist(), 2)
+        		frame = cv2.circle(imgbgr,(a,b),5,self.color[i].tolist(),-1)
+    		imgout = cv2.add(imgbgr,self.mask)
+
+		# Now update the previous frame and previous points
+    		self.old_gray = frame_gray.copy()
+   		self.p0 = good_new.reshape(-1,1,2)
+
+
+
+		#print p1[5][0][1] #the zero in the middle is required because the array is nominally 3 dimensional but one dimension has 0 thickness
+
+		imgdrone = self.bridge.cv2_to_imgmsg(imgbgr, "8UC3") #converts opencv's bgr8 back to the drone's raw_image for rviz use, converts both hsv and rgb to rviz-readable form
+
+		self.pub_image.publish(imgdrone)
+
+		imgdrone2 = self.bridge.cv2_to_imgmsg(imgout, "8UC3") #converts opencv's bgr8 back to the drone's raw_image for rviz use, converts both hsv and rgb to rviz-readable form
+
+		self.pub_image2.publish(imgdrone2)
 
 
 	def processimage1(self, imgdrone1):
